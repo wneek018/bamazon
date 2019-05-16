26344dfb8a45f822ee7d024241ce02a060b0a569
Wendy Wigg <wiggwendy@gmail.com>
2019-05-16 11:04:20 -0700

    working inquiry, order and update MySQL database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,6 +27,53 @@ function showProducts() {
             "Price: " + res[i].price + "\n");
         }
         // prompt the user for the product and quantity they want to order
-        //placeOrder();
+        placeOrder();
+    });
+}
+
+function placeOrder() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "productId",
+            message: "What is the product ID of the item you want to buy?"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to buy?"
+        }
+    ]).then(function(answer) {
+        connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+            var chosenProduct = "";
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_id === parseInt(answer.productId)) {
+                    chosenProduct = res[i];
+                }
+            }
+            // if there is enough quantity, fill the order
+            if (chosenProduct.stock_quantity >= parseInt(answer.quantity)) {
+                // update the MySQL database
+                connection.query("UPDATE products SET ? WHERE ?",[
+                    {
+                        stock_quantity: (chosenProduct.stock_quantity - parseInt(answer.quantity))
+                    },
+                    {
+                        item_id: chosenProduct.item_id
+                    }
+                ], function (err, res) {
+                    if (err) throw err;
+                    // log the affected rows to check mysql connection
+                    console.log(res.affectedRows + " products updated.");
+                    // show the customer the total cost of their purchase
+                    console.log("Your total is $" + (parseInt(answer.quantity) * chosenProduct.price));
+                })
+                // if there is not enough quantity left, do not fill the request
+            } else {
+                console.log("We don't have enough in stock to fulfill your request.");
+            }
+            connection.end();
+        });
     });
 }
