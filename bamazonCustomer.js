965bb2554eba6ab87fe7024f5de7def6499e115b
Wendy Wigg <wiggwendy@gmail.com>
2019-05-16 10:52:48 -0700

    showing results from mysql in node bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,4 +13,20 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    // show all products in MySQL database
+    showProducts();
 });
+
+function showProducts() {
+    console.log("Products Available: \n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item ID: " + res[i].item_id + "\n" +
+            "Product Name: " + res[i].product_name + "\n" +
+            "Price: " + res[i].price + "\n");
+        }
+        // prompt the user for the product and quantity they want to order
+        //placeOrder();
+    });
+}
